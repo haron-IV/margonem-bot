96b81e6b980df074e74e693133c1551a471df327
Venglas <bartek.bober97@gmail.com>
2019-02-27 02:40:08 +0100

    change styles for interface from js to css

--- extension/content-scripts/addBotInterface.js
+++ extension/content-scripts/addBotInterface.js
@@ -2,23 +2,29 @@ setTimeout(() => {
 
     const startBotButton = document.createElement('button');
     startBotButton.id = 'start-bot';
+    startBotButton.classList.add('start-bot');
     startBotButton.innerHTML = 'start';
-    startBotButton.style.fontSize = '10px';
-    document.querySelector('#nick').appendChild(startBotButton)
+    document.querySelector('#centerbox2').appendChild(startBotButton)
 
 
     const stopBotButton = document.createElement('button');
     stopBotButton.id = 'stop-bot';
+    stopBotButton.classList.add('stop-bot');
     stopBotButton.innerHTML = 'stop';
-    stopBotButton.style.fontSize = '10px';
-    document.querySelector('#nick').appendChild(stopBotButton)
+    document.querySelector('#centerbox2').appendChild(stopBotButton)
 
 
     const $buttonstart = document.querySelector('#start-bot');
 
 
-    $buttonstart.addEventListener('click', ()=>{
-        $buttonstart.style.backgroundColor = "green";
-    })
+    // $buttonstart.addEventListener('click', ()=>{
+    //     $buttonstart.style.backgroundColor = "green";
+    // });
+
+    chrome.storage.sync.get(['botStatus'], (botStats) => { // pobieranie wartosci total i limit z chrome storage do obiektu budget i wyswietlanie go
+        if ( botStats.botStatus ===  true ) {
+            $buttonstart.style.backgroundColor = 'green';
+        }
+    });
 
 }, 1500);
