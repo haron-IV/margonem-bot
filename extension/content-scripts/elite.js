1f4e5be2f67ec28f64b950eac2d4f30d5dfef186
Venglas <bartek.bober97@gmail.com>
2019-02-27 20:05:53 +0100

    add autofight closefight and reload for elite bot

--- extension/content-scripts/elite.js
+++ extension/content-scripts/elite.js
@@ -6,11 +6,34 @@ setTimeout(() => {
         
         npcs.forEach(el => {
             if( el.getAttribute('tip').split('elita').length >= 2 ) {
-            console.log('here is a elite', el);
-            el.click();
-            clearInterval(interval);
+                console.log('here is a elite', el);
+                el.click();
+                clearInterval(interval);
+                autoFight();
+                closeFight();
+                reload();
             }
         });
         
     }, 500);
+
+    function autoFight(){
+        setInterval(() => {
+          document.querySelector('#autobattleButton').click(); 
+        }, 3000);
+    }
+    
+    function closeFight(){
+        setInterval(() => {
+            if ( document.querySelector('#battleclose') ) {
+                document.querySelector('#battleclose').click();
+            }
+        }, 5500);
+    }
+
+    function reload(){
+        setTimeout(() => {
+            window.location.reload()
+        }, 7000);
+    }
 }, 2000);
