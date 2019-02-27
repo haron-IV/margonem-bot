8fb94e0456a238f66817eab9b43e90a3e2fd0718
Venglas <bartek.bober97@gmail.com>
2019-02-27 22:10:49 +0100

    refactorize styles and js for interface

--- extension/content-scripts/addBotInterface.js
+++ extension/content-scripts/addBotInterface.js
@@ -4,38 +4,29 @@ setTimeout(() => {
     wrapper.id = "bot-interface-wrapper";
     wrapper.classList.add('bot-interface-wrapper');
     document.querySelector('#base').appendChild(wrapper);
-
+    const $wrapper = document.querySelector('#bot-interface-wrapper');
 
     const startBotButton = document.createElement('button');
     startBotButton.id = 'start-bot';
-    startBotButton.classList.add('start-bot');
+    startBotButton.classList.add('start-bot', 'bot-button');
     startBotButton.innerHTML = 'start';
-
-    const $wrapper = document.querySelector('#bot-interface-wrapper');
-
-
     $wrapper.appendChild(startBotButton);
-
-
-
-
-
 
     const stopBotButton = document.createElement('button');
     stopBotButton.id = 'stop-bot';
-    stopBotButton.classList.add('stop-bot');
+    stopBotButton.classList.add('stop-bot', 'bot-button');
     stopBotButton.innerHTML = 'stop';
     $wrapper.appendChild(stopBotButton)
 
     const eliteBotButtonStart = document.createElement('button');
     eliteBotButtonStart.id = 'elite-bot-start';
-    eliteBotButtonStart.classList.add('elite-bot-start');
+    eliteBotButtonStart.classList.add('elite-bot-start', 'bot-button');
     eliteBotButtonStart.innerHTML = 'elite bot start';
     $wrapper.appendChild(eliteBotButtonStart);
 
     const eliteBotButtonStop = document.createElement('button');
     eliteBotButtonStop.id = 'elite-bot-stop';
-    eliteBotButtonStop.classList.add('elite-bot-stop');
+    eliteBotButtonStop.classList.add('elite-bot-stop', 'bot-button');
     eliteBotButtonStop.innerHTML = 'elite bot stop';
     $wrapper.appendChild(eliteBotButtonStop);
 
