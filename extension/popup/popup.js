a5d055a8c529be107c9afb23ebfa437610623788
Venglas <bartek.bober97@gmail.com>
2019-02-28 03:14:21 +0100

    update bot page

--- extension/popup/popup.js
+++ extension/popup/popup.js
@@ -11,33 +11,3 @@ chrome.storage.sync.get(['needExp', 'lastLocalization', 'lastLocalizationCoord',
     $gold.innerHTML = botStats.gold;
     $killedElites.innerHTML = botStats.killedElites;
 });
-
-// $enter_amount_button.addEventListener('click', () => {
-//     chrome.storage.sync.get(['total', 'limit'], function (budget){ // total - value in storage in budget object
-        // let newTotal = 0;
-        // if (budget.total){
-        //     newTotal += parseInt( budget.total );
-        // }
-
-        // let amount = $enter_amount.value;
-        // if(amount){
-        //     newTotal += parseInt( amount );
-        // }
-
-        // chrome.storage.sync.set({"total": newTotal}, () => {
-        //     if (amount && newTotal >= budget.limit){
-        //         const notificationOptions = { // tworzenie objektu konfiguracyjnego dla powiadomienia
-        //             type: "basic", // sa rozne typy powiadomien
-        //             iconUrl: "../icons/icon128.png",
-        //             title: "Limit reached!",
-        //             message: "Hey! You reached the limit :/"
-        //         };
-
-        //         chrome.notifications.create('limit_notification', notificationOptions); //wywołanie powiadomienia
-        //     }
-        // });
-
-        // $total_spend.innerHTML = newTotal;
-        // $enter_amount.value = '';
-//     } );
-// });
