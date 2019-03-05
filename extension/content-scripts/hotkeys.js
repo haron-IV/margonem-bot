a7a5801acdf11035daa22c873631e4f1e80a69b8
Venglas <bartek.bober97@gmail.com>
2019-03-05 13:26:04 +0100

    if user use the chat dont show him things from hotkeys

--- extension/content-scripts/hotkeys.js
+++ extension/content-scripts/hotkeys.js
@@ -1,57 +1,68 @@
-let friendsListActive = false;
+const data = {
+    friendsListActive: false,
+    mapActive: false,
+    questListActive: false,
+    chatEl: document.querySelector('#inpchat'),
+    chatIsUse: false
+}
+
+function friendsList(){
+    if ( data.friendsListActive === false && data.chatIsUse === false) {
+        document.querySelector('#b_friends').click();
+        data.friendsListActive = true;
+    } else if (data.friendsListActive === true && data.chatIsUse === false) {
+        document.querySelectorAll('.closebut')[2].click();
+        data.friendsListActive = false;
+    }    
+}
+
+function map(){
+    if ( data.mapActive === false && data.chatIsUse === false) {
+                
+        setTimeout(() => {
+            document.querySelector('#mappanel').children[0].children[0].click();    
+        }, 200);
+        
+        data.mapActive = true;
+    } else if (data.mapActive === true && data.chatIsUse === false) {
+        data.mapActive = false;
+    }  
+}
+
+function quest(){
+    if ( data.questListActive === false && data.chatIsUse === false) {
+        document.querySelector('#b_quests').click();
+        data.questListActive = true;
+    } else if (data.questListActive === true && data.chatIsUse === false) {
+        document.querySelector('.closebut').click();
+        data.questListActive = false;
+    }   
+}
 
 setTimeout(() => {
-    document.addEventListener('keyup', (e) => {
-        if(e.key === 'p'){
-            if ( friendsListActive === false ) {
-                document.querySelector('#b_friends').click();
-                friendsListActive = true;
-            } else {
-                document.querySelectorAll('.closebut')[2].click();
-                friendsListActive = false;
-            }    
-        }
-    } );    
-}, 2000);
-
-
-//
-let mapActive = false;
-
-setTimeout(() => {
-    document.addEventListener('keyup', (e) => {
-        if(e.key === 'm'){
-            
-            if ( mapActive === false ) {
-                
-                setTimeout(() => {
-                    document.querySelector('#mappanel').children[0].children[0].click();    
-                }, 150);
-                
-                mapActive = true;
-            } else {
-                mapActive = false;
-            }    
-        }
-    } );  
     
-}, 2000);
-
-//
-
-let questListActive = false;
-
-setTimeout(() => {
-    document.addEventListener('keyup', (e) => {
-        if(e.key === 'q'){
-            console.log(2)
-            if ( questListActive === false ) {
-                document.querySelector('#b_quests').click();
-                questListActive = true;
-            } else {
-                document.querySelector('.closebut').click();
-                questListActive = false;
-            }    
+    data.chatEl.addEventListener('keydown', (e) => {
+        if (e.key == 'Enter'){
+            data.chatIsUse = false;
+            console.log(data.chatIsUse)
+        } else {
+            data.chatIsUse = true;
         }
-    } );    
-}, 2000);
+    });
+
+    document.addEventListener('keyup', (e) => {
+        switch(e.key){
+            case 'p':
+                friendsList();
+            break;
+
+            case 'm':
+                map();
+            break;
+
+            case 'q':
+                quest();
+            break;
+        }
+    })
+}, 2500);
