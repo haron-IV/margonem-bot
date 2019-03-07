1d4b779915ee5fd36d0c17fa7ce3452ea28562dd
Venglas <bartek.bober97@gmail.com>
2019-03-07 10:14:42 +0100

    add searching quest to popup

--- extension/popup/popup.js
+++ extension/popup/popup.js
@@ -5,6 +5,8 @@ const $gold = document.querySelector('#gold');
 const $killedElites = document.querySelector('#killed-elites');
 const $search_npc_inp = document.querySelector('#npc-search-inp');
 const $search_npc_btn = document.querySelector('#npc-search-btn');
+const $search_quest_btn = document.querySelector('#quest-search-btn');
+const $search_quest_inp = document.querySelector('#quest-search-inp');
 
 chrome.storage.sync.get(['needExp', 'lastLocalization', 'lastLocalizationCoord', 'gold', 'killedElites'], (botStats) => { // pobieranie wartosci total i limit z chrome storage do obiektu budget i wyswietlanie go
     $need_exp.innerHTML = botStats.needExp;
@@ -19,3 +21,10 @@ $search_npc_btn.addEventListener('click', ()=>{
     const link = `http://emargo.pl/npc/szukaj?q=${npc_name}`;
     window.open(link, '_blank');
 });
+
+$search_quest_btn.addEventListener('click', () => {
+    const quest_name = $search_quest_inp.value;
+    const link = `http://emargo.pl/questy/szukaj?q=${quest_name}`;
+    window.open(link, '_blank');
+});
+
