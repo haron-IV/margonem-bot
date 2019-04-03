c4aa68e44cb3c27070a823b96326f29d02f4dd9b
Venglas <bartek.bober97@gmail.com>
2019-04-03 12:14:19 +0200

    add some for appearance lifting

---
+++ extension/content-scripts/changeFightWindow.js
@@ -0,0 +1,37 @@
+checkIsGameLoaded();
+
+function checkIsGameLoaded() {
+    let interval = setInterval(() => {
+        const loading_el = document.querySelector('#loading');
+        
+        if ( loading_el.style.display === '' ) {
+        } else if (loading_el.style.display === 'none') {
+            init_change_fight_window();
+            clearInterval(interval);
+        }
+    }, 1000);
+}
+
+function init_change_fight_window() {
+    getSettings();
+    changeFightWindow();
+}
+
+//
+let settings = {};
+//
+
+function getSettings(){
+    chrome.storage.sync.get(['settingsData'], (bot) => {
+        settings = bot.settingsData;
+    });
+}
+
+function changeFightWindow() {
+    setTimeout(() => {
+        if (settings.changed_fight_window === true){
+            document.querySelector('#battlelog').classList.add('fightLog');
+            document.querySelector('#battle').classList.add('battle');
+        }    
+    }, 250);
+}
