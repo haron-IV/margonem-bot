fac80f738c5934f5ff8df67859c7e194712e6c9e
Venglas <bartek.bober97@gmail.com>
2019-04-03 10:42:47 +0200

    add function for saving settings data

--- extension/popup/settings.js
+++ extension/popup/settings.js
@@ -1,7 +1,24 @@
 const $reset_black_list_btn = document.querySelector('#reset-black-list-btn');
+const $changed_fight_window = document.querySelector('#changed-fight-window');
+
+
+// 
+let settings = {
+    changed_fight_window: false
+};
+//
+
+function loadSettings(){
+    chrome.storage.sync.get(['settingsData'], (bot) => {
+        settings = bot.settingsData;
+        console.log('data: ', settings);
+    });
+}
+// loadSettings();
 
 
 $reset_black_list_btn.addEventListener('click', () => {
+    // clear black list
     chrome.storage.sync.get(['black_list_mob'], (mobs) => {
         const data = {
             data: []
@@ -9,4 +26,15 @@ $reset_black_list_btn.addEventListener('click', () => {
 
         chrome.storage.sync.set({'black_list_mob': data});
     });
+});
+
+$changed_fight_window.addEventListener('change', () => {
+
+    if ($changed_fight_window.checked === true){
+        settings.changed_fight_window = true
+    } else {
+        settings.changed_fight_window = false
+    }
+    
+    chrome.storage.sync.set({'settingsData': settings});
 });
