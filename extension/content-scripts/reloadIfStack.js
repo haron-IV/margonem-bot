81a716602f05833236521af97985c61b2ab111f9
Venglas <bartek.bober97@gmail.com>
2019-04-11 07:54:59 +0200

    update popup link for main bot page

--- extension/content-scripts/reloadIfStack.js
+++ extension/content-scripts/reloadIfStack.js
@@ -1,71 +1,71 @@
-const coordinates = {
-    old: getCoordinates(),
-    active: getCoordinates()
-}
+// const coordinates = {
+//     old: getCoordinates(),
+//     active: getCoordinates()
+// }
 
-function getCoordinates(){
-    return document.querySelector('#botloc').innerHTML;
-}
+// function getCoordinates(){
+//     return document.querySelector('#botloc').innerHTML;
+// }
 
-function updateOldCoordinates(){
-    coordinates.old = getCoordinates();
-}
+// function updateOldCoordinates(){
+//     coordinates.old = getCoordinates();
+// }
 
-function updateActiveCoordinates(){
-    coordinates.active = getCoordinates();
-}
+// function updateActiveCoordinates(){
+//     coordinates.active = getCoordinates();
+// }
 
-function updateCoordinates() {
-    setInterval(() => {
-        updateOldCoordinates();
-    }, 10000);
+// function updateCoordinates() {
+//     setInterval(() => {
+//         updateOldCoordinates();
+//     }, 10000);
 
-    setInterval(() => {
-        updateActiveCoordinates();
-        // console.log(coordinates)
-    }, 3731);
-}
+//     setInterval(() => {
+//         updateActiveCoordinates();
+//         // console.log(coordinates)
+//     }, 3731);
+// }
 
-function reload (message) {
-    console.log(message)
-    window.location.reload();
-}
+// function reload (message) {
+//     console.log(message)
+//     window.location.reload();
+// }
 
-function checkCoordinates(){
-    chrome.storage.sync.get(['botStatus'], (botStats) => {
-        if( botStats.botStatus === true && coordinates.old === coordinates.active ){
-            reload('Reload beacuse your hero is stuck.');
-        }
-    });
-}
+// function checkCoordinates(){
+//     chrome.storage.sync.get(['botStatus'], (botStats) => {
+//         if( botStats.botStatus === true && coordinates.old === coordinates.active ){
+//             reload('Reload beacuse your hero is stuck.');
+//         }
+//     });
+// }
 
-function isDead(){
-    if (document.querySelector('#dazed').style.display == "block"){
-        return true;
-    } else {
-        return false;
-    }
-}
+// function isDead(){
+//     if (document.querySelector('#dazed').style.display == "block"){
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
 
-function init_reloading() {
-    updateCoordinates();
+// function init_reloading() {
+//     updateCoordinates();
 
 
-    setInterval(() => {
-        checkCoordinates();
-    }, 15000);
+//     setInterval(() => {
+//         checkCoordinates();
+//     }, 15000);
 
-    setInterval(() => {
+//     setInterval(() => {
         
-        chrome.storage.sync.get(['botStatus'], (bot) => {
-            if (isDead() === true && bot.botStatus === true){
-                document.querySelector('#logoutbut').click();
-            }
-        });
+//         chrome.storage.sync.get(['botStatus'], (bot) => {
+//             if (isDead() === true && bot.botStatus === true){
+//                 document.querySelector('#logoutbut').click();
+//             }
+//         });
         
-    }, 45000);
+//     }, 45000);
 
-}
+// }
 
 function checkLoading() {
     let interval = setInterval(() => {
@@ -74,10 +74,42 @@ function checkLoading() {
         if ( loading_el.style.display === '' ) {
             console.log('loading')
         } else if (loading_el.style.display === 'none') {
-            init_reloading();
+            // init_reloading();
+            tests();
             clearInterval(interval);
         }
     }, 1500);
 }
 
 checkLoading();
+
+function tests(){
+    const data = {
+        coordinates_old: document.querySelector('#botloc').innerHTML,
+        coordinates_active: document.querySelector('#botloc').innerHTML,
+        distance: 0
+    }
+
+    setInterval(() => {
+        coordinates_old = document.querySelector('#botloc').innerHTML;
+    }, 1331);
+
+    setInterval(() => {
+        data.coordinates_active = document.querySelector('#botloc').innerHTML;
+    }, 888);
+
+    setInterval(() => {
+        // checkWalking(data.coordinates_old, data.coordinates_active, data.distance, data);
+        if (data.coordinates_old !== data.coordinates_active){
+            data.distance+=1;
+            console.log('Distance: ', data)
+        }
+    }, 2450);
+}
+
+function checkWalking(coordinates_old, coordinates_active, distance, dataV){
+    if (coordinates_old !== coordinates_active){
+        distance+=1;
+        console.log('Distance: ', dataV)
+    }
+}
